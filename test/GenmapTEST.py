55dcfb1a24b444e66125e5e0c8ac5119121e3040
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-05-08 16:50:13 +0700

    generate function

---
+++ test/GenmapTEST.py
@@ -0,0 +1,50 @@
+import pygame
+
+from config.config import INIT_WIDTH, CELLSIZE, INIT_HEIGHT, DARKGREY, FPS
+from object.Cell import Cell
+
+size = INIT_WIDTH // CELLSIZE, INIT_HEIGHT // CELLSIZE
+
+
+def remove_walls(current, next):
+    dx = current.x - next.x
+    if dx == 1:
+        current.walls['left'] = False
+        next.walls['right'] = False
+    elif dx == -1:
+        current.walls['right'] = False
+        next.walls['left'] = False
+    dy = current.y - next.y
+    if dy == 1:
+        current.walls['top'] = False
+        next.walls['bottom'] = False
+    elif dy == -1:
+        current.walls['bottom'] = False
+        next.walls['top'] = False
+
+
+def generate_maze():
+    rows, cols = size
+    grid_cells = [Cell(col, row, size) for row in range(rows) for col in range(cols)]
+    current_cell = grid_cells[0]
+    array = []
+    break_count = 1
+
+    while break_count != len(grid_cells):
+        current_cell.visited = True
+        next_cell = current_cell.check_neighbors(grid_cells)
+        if next_cell:
+            next_cell.visited = True
+            break_count += 1
+            array.append(current_cell)
+            remove_walls(current_cell, next_cell)
+            current_cell = next_cell
+        elif array:
+            current_cell = array.pop()
+    return grid_cells
+
+
+
+
+
+
