6e9525c5ef606961b252df21f34a4d35b4bc2a1e
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-06-17 21:44:14 +0700

    add ratio

--- config/logicConfig.py
+++ config/logicConfig.py
@@ -13,12 +13,20 @@ class Config:
         self.fps = FPS
         self.level = 1
         self.config = custom
+        self.ratio = 1
+        self.cellsize = self.cellsize_ratio()
 
     def cellsize_level(self):
         cellsize = CELLSIZE
         if self.level != 1:
             cell = self.heigh // CELLSIZE + 5 * self.level
             cellsize = self.heigh // cell
+        return cellsize
+
+    def cellsize_ratio(self, ratio=None):
+        if ratio is None:
+            ratio = self.ratio
+        cellsize = self.cellsize_level() * ratio
         return cellsize
 
     def config_load(self):
