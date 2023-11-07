93e1b455ef8c790847997b9484b9f3e38c882e52
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-11-07 22:21:28 +0700

    main menu logic

--- menu/menu.py
+++ menu/menu.py
@@ -5,22 +5,22 @@ import pygame
 from object.draw.UIElement import UIElement
 
 
-def main_menu(screen, click=False):
+# def main_menu(screen, click=False, callback={}):
+def main_menu(screen, click=False, callbacks=None):
     # draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
+
     UIElement(20, 20, 'main menu').draw(screen, font=pygame.font.Font('../assets/font/emulogic.ttf'))
 
     mx, my = pygame.mouse.get_pos()
-
-    button_1 = pygame.Rect(50, 100, 200, 50)
-    button_2 = pygame.Rect(50, 200, 200, 50)
-    if button_1.collidepoint((mx, my)):
-        if click:
-            pass
-    if button_2.collidepoint((mx, my)):
-        if click:
-            pass
-    pygame.draw.rect(screen, (255, 0, 0), button_1)
-    pygame.draw.rect(screen, (255, 0, 0), button_2)
+    y = 100
+    # for x, func in callback:
+    for callback in callbacks:
+        button = pygame.Rect(50, y, 200, 50)
+        y += 100
+        if button.collidepoint((mx, my)):
+            if click:
+                callback()
+        pygame.draw.rect(screen, (255, 0, 0), button)
 
     click = False
     for event in pygame.event.get():
@@ -34,5 +34,5 @@ def main_menu(screen, click=False):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 click = True
+        return click
 
-    return click
