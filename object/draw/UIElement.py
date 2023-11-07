b8fdffad418f06316498e4bc0334de80dd9bbbce
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-11-07 22:21:47 +0700

    addition element

--- object/draw/UIElement.py
+++ object/draw/UIElement.py
@@ -7,8 +7,15 @@ class UIElement:
     def __init__(self, x, y, text):
         self.x, self.y = x, y
         self.text = text
+        self.size = 30
 
-    def draw(self, screen, color=WHITE):
-        font = pygame.font.SysFont("Consolas", 30)
+    def draw(self, screen, size=None, font=None, color=WHITE):
+        if size is None:
+            size = self.size
+        if font is None:
+            font = pygame.font.SysFont("Consolas", size)
+
         text = font.render(self.text, True, color)
         screen.blit(text, (self.x, self.y))
+    def click(self, mouse_x, mouse_y):
+        return self.x <= mouse_x <= self.x + self.width and self.y <= mouse_y <= self.y + self.height
