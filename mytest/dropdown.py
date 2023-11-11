3e633f6c94278ef683b80b95037abf328f09fbde
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-11-11 20:44:37 +0700

    pygame_gui add

--- mytest/dropdown.py
+++ mytest/dropdown.py
@@ -1,12 +1,13 @@
 import pygame
 
+from pygame_gui import UIManager
 
 pygame.init()
 
 test_surface = pygame.display.set_mode((800, 600), 0, 32)
 test_surface.fill(pygame.Color(128, 128, 128, 255))
 
-
+manager = UIManager((800, 600))
 
 options_list = [f"option {x}" for x in range(3)]
 
@@ -19,7 +20,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        manager.process_events(event)
 
+    manager.update(0.001)
     time += 0.001
-
+    manager.draw_ui(test_surface)
     pygame.display.update()
