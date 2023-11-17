7d4164fc1b9383cff4efdf08d2c971b2767d1436
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-11-17 22:46:03 +0700

    greedySolve.py fix

--- Algorithm/greedySolve.py
+++ Algorithm/greedySolve.py
@@ -1,48 +1,56 @@
 import pygame
 from config.logicConfig import Config
 
+def evaluate(cell, goal):
+    return ((cell.x - goal.x) ** 2 + (cell.y - goal.y) ** 2) ** 0.5
 
 def greedy(maze, start, goal, sc, config=None):
     if config is None:
         config = Config()
     print('hear')
-    queue = [(start, [])]  # Initialize the queue with the start cell and an empty path
 
-    while queue:
-        current_cell, path = queue[
-            0]  # Get the current cell and its path (Greedy selects the "best" based on heuristic)
+    current_cell = start
+    path = [current_cell]
+    found_path = False
 
-        if current_cell == goal:
-            [cell.draw_current_cell(sc) for cell in path]
-            # We found the goal, return the path
-            return path
+    while current_cell != goal:
+        current_cell.visited = True
+        neighbors = current_cell.find_neighbors(maze)
 
-        if not current_cell.visited:
-            current_cell.visited = True
+        best_neighbor = None
+        best_score = float('inf')
 
-            # Get neighboring cells
-            neighbors = current_cell.find_neighbors(maze)
+        for neighbor in neighbors:
+            if not neighbor.visited:  # Đảm bảo không quay lại ô đã đi qua
+                score = evaluate(neighbor, goal)
+                if score < best_score:
+                    best_score = score
+                    best_neighbor = neighbor
 
-            # Sort neighbors by a heuristic (e.g., distance to the goal)
-            neighbors.sort(key=lambda neighbor: heuristic(neighbor, goal))
+        if best_neighbor is None:
+            # Không tìm thấy đường đi, quay lại bước trước đó
+            if len(path) > 1:  # Nếu có ít nhất 2 ô trong đường đi
+                path.pop()  # Bỏ ô hiện tại
+                current_cell = path[-1]  # Đặt ô hiện tại là ô trước đó
+            else:
+                # Nếu không còn ô nào để quay lại, thoát vòng lặp vô hạn
+                break
+        else:
+            # Tìm thấy ô tiếp theo, thêm vào đường đi
+            path.append(best_neighbor)
+            current_cell = best_neighbor
 
-            for neighbor in neighbors:
-                new_path = path + [current_cell]
-                queue.append((neighbor, new_path))
-
+        [cell.draw_current_cell(sc) for cell in path]
         [cell.draw(sc) for cell in maze]
         pygame.display.flip()
         pygame.time.Clock().tick(20)
         pygame.display.update()
 
-        queue.pop(0)  # Remove the first element (Greedy doesn't use a deque)
+        if current_cell == goal:
+            found_path = True
+            break
 
-    # If no path is found, return None to indicate that there's no solution
-    return None
-
-
-def heuristic(cell, goal):
-    # Replace with your specific heuristic function, e.g., Manhattan distance
-    return abs(cell.x - goal.x) + abs(cell.y - goal.y)
-
-# The remaining code (import statements and non-algorithm-related parts) remains unchanged.
+    if found_path:
+        return path  # Trả về đường đi nếu tìm thấy
+    else:
+        return None  # Trả về None nếu không tìm thấy đường đi
