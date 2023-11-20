7a015f83ac24a76f98c9f346ae7b110ebad97910
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-11-21 06:50:35 +0700

    arrange

--- object/gameplay/Cell.py
+++ object/gameplay/Cell.py
@@ -104,6 +104,10 @@ class Cell:
         right = self.check_cell(self.x + 1, self.y)
         bottom = self.check_cell(self.x, self.y + 1)
         left = self.check_cell(self.x - 1, self.y)
+        if left and not left.visited and not self.walls['left']:
+            neighbors.append(left)
+        if right and not right.visited and not self.walls['right']:
+            neighbors.append(right)
         if top and not top.visited and not self.walls['top']:
             neighbors.append(top)
         if right and not right.visited and not self.walls['right']:
@@ -115,3 +119,51 @@ class Cell:
         return neighbors
 
     def click(self, pos_cell, wall_remove=False, grid_cells=None):
+        self.grid_cells = grid_cells
+        x, y, px_cell, py_cell = pos_cell
+        # print(str(self.x) + ' ' + str(self.x + self.cel_size) + ' ' + str(self.y) + ' ' + str(self.y + self.cel_size))
+        # print(str(mouse_x), mouse_y)
+        # is_click = self.x <= mouse_x/self.cel_size <= self.x + self.cel_size and self.y <= mouse_y/self.cel_size <= self.y + self.cel_size
+        is_click = self.x == x and self.y == y
+        if is_click:
+            print('clicked', self.x, self.y)
+            # top = left = right = bottom = False
+            top = self.check_cell(self.x, self.y - 1)
+            right = self.check_cell(self.x + 1, self.y)
+            bottom = self.check_cell(self.x, self.y + 1)
+            left = self.check_cell(self.x - 1, self.y)
+            change = []
+            if wall_remove and is_click:
+                if 0 < px_cell < 0.2 and left:
+                    print('click left')
+                    self.walls['left'] = not self.walls['left']
+                    change.append((x, y))
+                    # if left:
+                    left.walls['right'] = not left.walls['right']
+                    change.append((x-1, y))
+                if 0.8 < px_cell < 1 and right:
+                    print('click right')
+                    self.walls['right'] = not self.walls['right']
+                    change.append((x, y))
+                    # if right:
+                    right.walls['left'] = not right.walls['left']
+                    change.append((x+1, y))
+                if 0 < py_cell < 0.2 and top:
+                    print('click top')
+                    self.walls['top'] = not self.walls['top']
+                    change.append((x, y))
+                    # if top:
+                    top.walls['bottom'] = not top.walls['bottom']
+                    change.append((x, y-1))
+                if 0.8 < py_cell < 1 and bottom:
+                    print('click bottom')
+                    self.walls['bottom'] = not self.walls['bottom']
+                    change.append((x, y))
+                    # if bottom:
+                    bottom.walls['top'] = not bottom.walls['top']
+                    change.append((x, y+1))
+                return True, change
+        return is_click, []
+
+    def __lt__(self, other):
+        return self.x + self.y * self.size[0] > other.x + other.y * other.size[0]
